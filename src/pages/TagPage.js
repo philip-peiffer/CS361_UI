c5f45576c01be514e4f21020f8ff3498166c0d77
philip-peiffer <philip.peiffer@gmail.com>
2022-05-23 11:20:49 -0600

    added success rate graph to tag page

--- src/pages/TagPage.js
+++ src/pages/TagPage.js
@@ -7,27 +7,32 @@ import YearComparisonChart from '../components/TagPage/YearComparisonChart'
 
 function TagPage({selections, setSelections}){
     const [tagData, setTagData] = useState([])
-    const [lineData, setLineData] = useState({'applicants': [{'loading...': 0}], 'successes': [{'loading...': 0}], 'pts spent': [{'loading...': 0}]})
+    const [lineData, setLineData] = useState({'applicants': [{'loading...': 0}], 'success rate': [{'loading...': 0}], 'pts/app (avg)': [{'loading...': 0}], 'pts spent': [{'loading...': 0}]})
     const lineCats = Object.keys(lineData)
 
     function reformatData (data) {
         // this function reformats the data so that the line charts can be used. Line charts need the data in format
         // where every tag's stat that you want displayed is grouped by year. Currently data is grouped by tag and then year
-        let returnDict = {'applicants': [], 'successes': [], 'pts spent': []}
+        let returnDict = {'applicants': [], 'success rate': [], 'pts/app (avg)': [], 'pts spent': []}
 
         data[0].years.forEach(year => {
             let i = year - data[0].years[0]
             let applicantDict = {'year': year}
             let successDict = {'year': year}
+            let ptperappDict = {'year': year}
             let ptsDict = {'year': year}
+            
             data.map(dataPoint => {
-                applicantDict[dataPoint.tag] = dataPoint['year stats'][i]['applicants']
-                successDict[dataPoint.tag] = dataPoint['year stats'][i]['successes']
-                ptsDict[dataPoint.tag] = dataPoint['year stats'][i]['pts spent']
+                let yearStat = dataPoint['year stats'][i]
+                applicantDict[dataPoint.tag] = yearStat['applicants']
+                successDict[dataPoint.tag] = yearStat['% success']
+                ptperappDict[dataPoint.tag] = yearStat['avg pt per app']
+                ptsDict[dataPoint.tag] = yearStat['pts spent']
             })
 
             returnDict.applicants.push(applicantDict)
-            returnDict.successes.push(successDict)
+            returnDict['success rate'].push(successDict)
+            returnDict['pts/app (avg)'].push(ptperappDict)
             returnDict['pts spent'].push(ptsDict)
         })
         setLineData(returnDict)
@@ -51,7 +56,7 @@ function TagPage({selections, setSelections}){
             <SearchBar selections={selections} setSelections={setSelections}/>
             {lineCats.map((cat, index) => {
                 return (
-                <YearComparisonChart key={index} data={lineData[cat]} category={cat} />
+                    <YearComparisonChart key={index} data={lineData[cat]} category={cat} />
                 )
             })}
             {tagData.map((data, index) => {
