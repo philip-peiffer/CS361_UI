07afd1543cc63fa7ca38935da7ba6012f3f6331d
philip-peiffer <philip.peiffer@gmail.com>
2022-06-01 10:07:56 -0600

    added axis labels

--- src/components/TagPage/PointBarChart.js
+++ src/components/TagPage/PointBarChart.js
@@ -1,16 +1,16 @@
 import React from "react";
-import {BarChart, Bar, Legend, CartesianGrid, Tooltip, XAxis, YAxis} from 'recharts'
+import {BarChart, Bar, Legend, CartesianGrid, Tooltip, XAxis, YAxis, Label} from 'recharts'
 
 function PointBarChart({data}) {
     return (
         <div className="pointBarChart">
-            <BarChart width={750} height={500} data={data}>
+            <BarChart width={750} height={500} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="points" />
-                <YAxis yAxisId="left" orientation="left"/>
-                <YAxis yAxisId="right" orientation="right" stroke="#f76565"/>
+                <XAxis dataKey="points" label={{ value: 'Point Category', position: 'insideBottom' }}/>
+                <YAxis yAxisId="left" orientation="left" label={{ value: 'Num People', angle: -90, position: 'insideLeft' }}/>
+                <YAxis yAxisId="right" orientation="right" stroke="#f76565" label={{ value: 'Points', angle: -90, position: 'insideRight' }}/>
                 <Tooltip />
-                <Legend />
+                <Legend verticalAlign="insideTop"/>
                 <Bar yAxisId="left" dataKey="applicants" fill="#8884d8" />
                 <Bar yAxisId="left" dataKey="successes" fill="#82ca9d" />
                 <Bar yAxisId="right" dataKey="pts spent" fill="#f76565" />
